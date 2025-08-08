68d388c468e771c91bddf32a3fc70cb9c4bd5548
Milan Singh <milansinghthakur024@gmail.com>
2025-08-08 10:53:44 +0530

    updated the requriements file

--- app/main.py
+++ app/main.py
@@ -21,7 +21,8 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000"
+        "http://localhost:3000",
+        "ofm-chatbot.netlify.app",
     ],  # IMPORTANT: For production, restrict this to your frontend's actual URL
     allow_credentials=True,
     allow_methods=["*"],
